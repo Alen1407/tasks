37bfa88c60722fb920f15ddfd57b5b835746d2fd
Alen <114245751+Alen1407@users.noreply.github.com>
2023-06-16 18:48:49 +0400

    main.py

--- CubeEncryption/main.py
+++ CubeEncryption/main.py
@@ -1,16 +1,7 @@
-from Cube import Cube
 from Encryption import Encryption
 from Decryption import Decryption
 
-text = "students"
-
-cube = Cube(text)
-
-cube.rotate_left()
-cube.rotate_right()
-cube.rotate_up()
-cube.rotate_down()
 
 enc = Encryption("../file")
-dec = Decryption(enc.encrypt(), "U3:R3:D3")
+dec = Decryption("../encrypted")
 print(dec.decrypt())
